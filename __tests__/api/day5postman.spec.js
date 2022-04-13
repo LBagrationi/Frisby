363631cac8e7cf9ac8aaa7ffe9d4fe03974ec668
LBagrationi <101694500+LBagrationi@users.noreply.github.com>
2022-04-13 14:04:29 +0400

    Update day5postman.spec.js

--- __tests__/api/day5postman.spec.js
+++ __tests__/api/day5postman.spec.js
@@ -5,12 +5,9 @@ describe("Day 05", () => {
       var currency = 'usd';
       return frisby.get('https://api.coindesk.com/v1/bpi/currentprice/' + currency + '.json')
         .expect("status", 200)
-        /*.expect("jsonTypes", "disclaimer", Joi.string().required())
-        .expect("jsonTypes", "time", Joi.object().required())*/
         .then((result) => {
           const body = result.json;
           console.log(result.time);
-          //console.log: undefined (?)
         });
     });
-    });
+});
